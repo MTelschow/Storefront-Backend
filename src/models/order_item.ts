6932048ca1d466f962947257a3b902c685feae35
Mathis Telschow <mathistelschow@gmail.com>
2024-07-03 19:08:24 +0200

    Add order items model

---
+++ src/models/order_item.ts
@@ -0,0 +1,87 @@
+// @ts-ignore
+import Client from "../database";
+
+export type OrderItem = {
+  id: Number;
+  order_id: Number;
+  product_id: Number;
+  quantity: Number;
+};
+
+export class OrderStore {
+  async index(): Promise<OrderItem[]> {
+    try {
+      // @ts-ignore
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM order_items";
+
+      const result = await conn.query(sql);
+      console.log(result);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get order items. Error: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<OrderItem> {
+    try {
+      const sql = "SELECT * FROM order_items WHERE id=($1)";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not find order item ${id}. Error: ${err}`);
+    }
+  }
+
+  async create(order_item: OrderItem): Promise<OrderItem> {
+    try {
+      const sql =
+        "INSERT INTO order_items (order_id, product_id, quantity) VALUES($1, $2, $3) RETURNING *";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [
+        order_item.order_id,
+        order_item.product_id,
+        order_item.quantity,
+      ]);
+
+      const product = result.rows[0];
+
+      conn.release();
+
+      return product;
+    } catch (err) {
+      throw new Error(
+        `Could not add new order item ${order_item.id}. Error: ${err}`
+      );
+    }
+  }
+
+  async delete(id: string): Promise<OrderItem> {
+    try {
+      const sql = "DELETE FROM order_items WHERE id=($1)";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      const product = result.rows[0];
+
+      conn.release();
+
+      return product;
+    } catch (err) {
+      throw new Error(`Could not delete order ${id}. Error: ${err}`);
+    }
+  }
+}
