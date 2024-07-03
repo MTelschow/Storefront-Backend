38b3d030f819fce989c99d4975e54b7030a8a378
Mathis Telschow <mathistelschow@gmail.com>
2024-07-03 18:46:07 +0200

    Add orders and order_items migrations

--- migrations/sqls/20240703144745-orders-up.sql
+++ migrations/sqls/20240703144745-orders-up.sql
@@ -1,9 +1,6 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY UNIQUE NOT NULL,
     user_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    active BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    status VARCHAR(10) DEFAULT 'active',
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
---
+++ migrations/sqls/20240703164237-order-items-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
