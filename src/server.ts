4b2fe7f1d7500f4808303f60f555f2773d66db1e
Mathis Telschow <mathistelschow@gmail.com>
2024-07-03 16:55:28 +0200

    Add basic server with express

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+dotenv.config();
 
-app.use(bodyParser.json())
+const port = process.env.PORT;
+const app: express.Application = express();
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+app.get("/", function (req: Request, res: Response) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`starting app on: port ${port}`);
+});
