a8cc756c6bad65774fa306c84a780c028e095192
Mathis Telschow <mathistelschow@gmail.com>
2024-07-09 16:43:09 +0200

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import user_routes from "./handlers/users";
 import product_routes from "./handlers/product";
 
 dotenv.config();
 
-const port = process.env.PORT;
 const app: express.Application = express();
+const port = process.env.PORT;
 
+const corsOptions = {
+  origin: process.env.CORSE_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-
-app.get("/", async function (req: Request, res: Response) {
-  res.send("Server working");
-});
 
 user_routes(app);
 product_routes(app);
