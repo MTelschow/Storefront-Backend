1e036afdfb5247134b8d432414655d8c1d11bce3
Mathis Telschow <mathistelschow@gmail.com>
2024-07-09 16:11:31 +0200

    Add product and user routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import user_routes from "./handlers/users";
+import product_routes from "./handlers/product";
 
 dotenv.config();
 
@@ -9,10 +11,13 @@ const app: express.Application = express();
 
 app.use(bodyParser.json());
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("Hello World!");
+app.get("/", async function (req: Request, res: Response) {
+  res.send("Server working");
 });
 
+user_routes(app);
+product_routes(app);
+
 app.listen(port, function () {
-  console.log(`starting app on: port ${port}`);
+  console.log(`starting app at:  localhost:${port}`);
 });
